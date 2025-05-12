874bafc5fb4c8522b3edacd6d97f07193d910141
Luis Mayta <luis@hadenlabs.com>
2025-05-11 21:03:11 -0300

    docs 📚(core): change information dependencies for packages (#38)
    Signed-off-by: Luis Mayta <luis@hadenlabs.com>

--- config/base.zsh
+++ config/base.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export DOCKER_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/zsh-brew branch:develop"
-export DOCKER_MESSAGE_YAY="Please install Go or use antibody bundle hadenlabs/zsh-goenv branch:develop"
-export DOCKER_MESSAGE_RVM="Please install rvm or use antibody bundle hadenlabs/zsh-rvm branch:develop"
-export DOCKER_MESSAGE_NVM="Please install nvm or use antibody bundle hadenlabs/zsh-nvm branch:develop"
+export DOCKER_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/zsh-brew"
+export DOCKER_MESSAGE_YAY="Please install Go or use antibody bundle hadenlabs/zsh-goenv"
+export DOCKER_MESSAGE_RVM="Please install rvm or use antibody bundle hadenlabs/zsh-rvm"
+export DOCKER_MESSAGE_NVM="Please install nvm or use antibody bundle hadenlabs/zsh-nvm"
