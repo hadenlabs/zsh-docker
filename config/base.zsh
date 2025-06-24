34d2917890d39b4ca0c1bf2759b22daca5a97b6b
Luis Mayta <luis@hadenlabs.com>
2025-06-23 22:05:28 -0300

    feat ✨ (core): implement var for container app name (#41)
    Signed-off-by: Luis Mayta <luis@hadenlabs.com>

--- config/base.zsh
+++ config/base.zsh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 export ZSH_DOCKER_PACKAGE_NAME=docker
-export ZSH_DOCKER_CONTAINER_APP_NAME="podman"
+export ZSH_DOCKER_CONTAINER_APP_NAME="${CONTAINER_APP_NAME:-podman}"
 export ZSH_DOCKER_PODMAN_MACHINE_NAME="podman-machine-default"
 export ZSH_DOCKER_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/zsh-brew"
 export ZSH_DOCKER_MESSAGE_YAY="Please install Go or use antibody bundle hadenlabs/zsh-goenv"
